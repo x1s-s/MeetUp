98d3b01ed691e36b2d64bdec450eda2cf0875ebb
Aliaksei Shkurko <shaleprog@gmail.com>
2022-10-15 19:33:10 +0300

    refactor: replace datasource

--- src/main/java/by/x1ss/utils/FlyWayMigration.java
+++ src/main/java/by/x1ss/utils/FlyWayMigration.java
@@ -1,14 +1,12 @@
 package by.x1ss.utils;
 
-import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 
-@Slf4j
+
 public class FlyWayMigration {
-    public static void flywayStartMigration(){
-        var flyway = Flyway.configure()
-                .dataSource("jdbc:postgresql://localhost:5432/MeetUp","postgres","6321")
-                .locations("classpath:/db/migration")
+    public static void flywayStartMigration() {
+        Flyway flyway = Flyway.configure().
+                dataSource("jdbc:postgresql://localhost:5432/MeetUp", "postgres", "6321")
                 .load();
         flyway.migrate();
     }
