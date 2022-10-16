58df33559d80000cb605a2f039278455879bd88c
Aliaksei Shkurko <shaleprog@gmail.com>
2022-10-16 11:06:42 +0300

    fix: edit jdbc connection url

--- src/main/java/by/x1ss/utils/FlyWayMigration.java
+++ src/main/java/by/x1ss/utils/FlyWayMigration.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.Flyway;
 public class FlyWayMigration {
     public static void flywayStartMigration() {
         Flyway flyway = Flyway.configure().
-                dataSource("jdbc:postgresql://localhost:5432/MeetUp", "postgres", "6321")
+                dataSource("jdbc:postgresql://postgres:5432/MeetUp", "postgres", "6321")
                 .load();
         flyway.migrate();
     }
